aca0420f2bdc3a9874b388ea1df595c070f3c7df
liubaichuan <1139058537@qq.com>
2018-11-01 21:23:53 +0800

    实现了初始化页面的时候通过后台获取图片数据的功能，但是数据仍然是通过datas.json生成的

--- GraphDemo/src/main/java/com/demo/controller/GraphController.java
+++ GraphDemo/src/main/java/com/demo/controller/GraphController.java
@@ -1,9 +1,10 @@
 package com.demo.controller;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,55 +18,74 @@ import com.demo.entity.VertexObj;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-
 @RestController
 public class GraphController {
 
 	@RequestMapping("/getAllGraph")
 	@ResponseBody
-	public Map<String, List<Object>> getAllGraph() throws IOException {
-//		List<Object> li1 = new ArrayList<Object>();
-//		List<Object> li2 = new ArrayList<Object>();
+	public Map<String, List<Object>> getAllGraph() {
 		Map<String, List<Object>> result = new HashMap<String, List<Object>>();
-//		for (int i = 1; i < 11; i++) {
-//			li1.add(new Vertex("u" + i, "n" + i, "d" + i));
-//			li2.add(new Vertex("u2" + i, "n2" + i, "d2" + i));
-//		}
-//		result.put("data", li1);
-//		result.put("link", li2);
-		File file = new File("datas.json");// 定义一个file对象，用来初始化FileReader
-		FileReader reader = new FileReader(file);// 定义一个fileReader对象，用来初始化BufferedReader
-		BufferedReader bReader = new BufferedReader(reader);// new一个BufferedReader对象，将文件内容读取到缓存
-		StringBuilder sb = new StringBuilder();// 定义一个字符串缓存，将字符串存放缓存中
-		String s = "";
-		while ((s = bReader.readLine()) != null) {// 逐行读取文件内容，不读取换行符和末尾的空格
-			sb.append(s + "\n");// 将读取的字符串添加换行符后累加存放在缓存中
+		// 读取点数据
+		File file = new File("datas_vertex.json");
+		Long filelength = file.length();
+		byte[] fileContent = new byte[filelength.intValue()];
+		try {
+			FileInputStream in = new FileInputStream(file);
+			in.read(fileContent);
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		bReader.close();
-		String str = sb.toString();
-
+		String strFileContent = null;
+		try {
+			strFileContent = new String(fileContent, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ObjectMapper mapper = new ObjectMapper();
-		List<Object> vertexs = mapper.readValue(str, new TypeReference<List<VertexObj>>() {
-		});
-		result.put("datas", vertexs);
-		
-		 file = new File("datas2.json");
-		 reader = new FileReader(file);
-		 bReader = new BufferedReader(reader);
-		 sb = new StringBuilder();
-		 s = "";
-		while ((s = bReader.readLine()) != null) {
-			sb.append(s + "\n");
-		}
-		bReader.close();
-		 str = sb.toString();
+		List<Object> vertexs = null;
+		try {
+			vertexs = mapper.readValue(strFileContent, new TypeReference<List<VertexObj>>() {
+			});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 
-		 mapper = new ObjectMapper();
-		List<Object> edges = mapper.readValue(str, new TypeReference<List<EdgeObj>>() {
-		});
-		
+		// 读取边数据
+		file = new File("datas_edge.json");
+		filelength = file.length();
+		fileContent = new byte[filelength.intValue()];
+		try {
+			FileInputStream in = new FileInputStream(file);
+			in.read(fileContent);
+			in.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			strFileContent = new String(fileContent, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		List<Object> edges = null;
+		try {
+			edges = mapper.readValue(strFileContent, new TypeReference<List<EdgeObj>>() {
+			});
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+
+		// 封装返回数据
+		result.put("datas", vertexs);
 		result.put("links", edges);
-		
 		return result;
 
 	}
