982ece8186b027ffc379a4927bd7aaf028b68c2c
purpletastes <573546735@qq.com>
2018-11-05 17:12:20 +0800

    搜索接口的post请求添加

--- GraphDemo/src/main/webapp/static/js/search.js
+++ GraphDemo/src/main/webapp/static/js/search.js
@@ -1,18 +1,37 @@
 function searchForm1(){
     var obj = {};
     var arrs = $("#searchForm").serializeArray();
+    obj.option = "all";
+    obj.vertexIds = [];
     $.each(arrs, function(index,arr) {
-        obj[arr.name] = arr.value;
+        if(arr.value){
+            obj.vertexIds .push(arr.value);
+        }
     });
+    loadData(obj);
+
+}
+function searchShortPath(arr){
+    var obj = {};
+    obj.option = "shortest";
+    obj.vertexIds = arr;
+    loadData(obj);
+}
+function loadData(obj) {
     var data = JSON.stringify(obj);
-    console.log(data);
     $.ajax({
         type: 'post',
-        url: 'search',
+        url: '/searchPath',
         contentType: 'application/json;charset=utf-8',
         data: data,
         success: function (response) { //返回json结果
             console.log(response)
+            if(obj.option ==="all"){
+                //更新节点关系图
+            }else{
+                //展示最短路径
+            }
+
         }
     })
 }
